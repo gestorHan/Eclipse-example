85b774ab0c3b22f127c9a3ba82440aaaff89a6d6
Jesus <torrele.09@gmail.com>
2020-11-02 22:09:02 -0500

    added new line and comment

--- Example-program/src/demo/welcome.java
+++ Example-program/src/demo/welcome.java
@@ -5,6 +5,12 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		//comentarios 
+		System.out.println("Segunda linea");
+		
+		
 	}
+	
+	
 
 }
